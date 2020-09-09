307784fc2f68e6f31dd89763ea26338aae11ca99
Surov Konstantin <q05t9n@gmail.com>
2020-09-09 22:08:58 +0300

    1-components_3-category-product-card

--- 1-components/2-footer/src/app/footer/footer.component.ts
+++ 1-components/2-footer/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-footer',
--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,11 @@
 import {
   Component,
   ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
 } from '@angular/core';
+import { IProduct } from 'shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +13,15 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Input() public product: IProduct = {} as IProduct;
+  @Output() addToCart = new EventEmitter<void>();
+  @Output() goToProduct = new EventEmitter<void>();
 
+  public addToBasket(): void {
+    this.addToCart.emit();
+  }
 
+  public redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
