ed1e0a85edcaa6f3b136d0f68d5aa35aecb3b73e
Surov Konstantin <q05t9n@gmail.com>
2020-09-09 22:18:56 +0300

    1-components_4-suggested-product

--- 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { IProduct } from 'shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-product-card',
   templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+  @Input() public product: IProduct = {} as IProduct;
+  @Output() goToProduct = new EventEmitter<void>();
 
+  public redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
